dc0312b1f5761818bd24aab5b9729fe43955cea4
SkylynxX <meshcheriakova.elizaveta@gmail.com>
2022-11-11 02:52:01 +0300

    add utils

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,32 @@
+export class MoviesApi {
+  constructor(inObj) {
+    this._baseUrl = inObj.baseUrl;
+    this._headers = inObj.headers;
+  }
+  //Общая обработка результата запроса: ok - возвращаем сериализованный ответ, not ok - возвращаем промис со статусом  (аргумент - объект Response возвращемый промисом fetch)
+  _returnStatus(res) {
+    if (res.ok) {
+      // console.log(res)
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  }
+  
+  // Получить данные начальные о всех картах
+  getInitialMoviess() {
+    return fetch(`${this._baseUrl}/beatfilm-movies`, {
+      headers: this._headers,
+    }).then(this._returnStatus);
+  }
+}
+
+//экземпляр класса для экпорта
+const moviesAPI = new MoviesApi({
+  baseUrl: "https://api.nomoreparties.co",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default moviesAPI;
